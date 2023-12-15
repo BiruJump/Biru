1a7a63d81f09fedf6d7d9f8b427319af481e19ca
Birujump <aponjaen@gmail.com>
2023-12-15 08:56:17 +0700

    commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php include 'header.php';?>
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container text-center">
+        <img class="rounded-circle" width="300" height="300" src="https://upload.wikimedia.org/wikipedia/commons/c/cd/Sherlock_Holmes_Portrait_Paget.jpg" alt="">
+      <h1 class="display-4">Sherlock Holmes</h1>
+      <p class="lead">Sherlock Holmes is a fictional detective character created by Sir Arthur Conan Doyle , a Scottish author and doctor . Holmes, who calls himself a "consulting detective," is known for his sharp logical reasoning, ability to disguise himself, and skill in using forensic science to solve various cases.</p>
+    </div>
+  </div>
+
+
+  
+<div class="container text-center" id="about">
+
+<div class="row">
+<div class="col">
+<h1>About</h1>
+</div>
+</div>
+
+<div class="row">
+    <div class="col text-justify">
+        Narasi Watson menggambarkan Holmes sebagai karakter yang sangat kompleks dan pemurung, meskipun memiliki kebiasaan yang ketat, namun sangat tidak rapi. Tempat tinggalnya di London di 221B, Baker Street, dirawat oleh pengurus rumah tangganya, Ny. Hudson. Holmes tampaknya mengalami serangan mania dan depresi, yang disertai dengan merokok pipa, bermain biola, dan penggunaan kokain . Sepanjang empat novel dan 56 cerita pendek yang menampilkan Holmes, sejumlah karakter muncul kembali, termasuk inspektur Scotland Yard Lestrade yang kikuk ; kelompok “orang Arab jalanan” yang dikenal sebagai Irregular Baker Street, yang secara rutin dipekerjakan oleh Holmes sebagai informan; saudaranya yang lebih bijaksana namun kurang ambisius, Mycroft; dan, yang paling menonjol, lawannya yang tangguh ,Profesor James Moriarty , yang dianggap Holmes sebagai “Napoleon kejahatan.”
+    </div>
+
+    <div class="col text-justify">
+        Mengklaim bahwa Holmes mengalihkan perhatiannya “dari hal-hal yang lebih baik,” Conan Doyle terkenal pada tahun 1893 (“Masalah Terakhir”) berusaha membunuhnya; selama perjuangan sengit di Air Terjun Reichenbach Swiss , Holmes dan musuh bebuyutannya , Profesor Moriarty, terjatuh ke tepi jurang . Kemarahan masyarakat terhadap kematian Holmes sangat besar; laki-laki mengenakan pita berkabung hitam, keluarga kerajaan Inggris putus asa , dan lebih dari 20.000 pembaca membatalkan langganan mereka ke Majalah Strand yang populer , tempat Holmes sering muncul. Atas permintaan banyak orang, Conan Doyle menghidupkan kembali detektifnya dalam cerita “Petualangan Rumah Kosong” (1903).
+    </div>
+
+</div>
+
+
+<div class="card-deck mt-5 ml-5 ml-5 mr-5">
+    <div class="card">
+      <img class="card-img-top" src="https://asset.kompas.com/crops/20sV5F63ZwAXqmSmVLcujja7HGI=/0x0:900x600/750x500/data/photo/2023/05/24/646dcdf051216.png" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Sang Author</h5>
+        <p class="card-text col text-justify">Sir Arthur Ignatius Conan Doyle KStJ DL (22 Mei 1859 – 7 Juli 1930) adalah pengarang cerita fiksi terkenal berkebangsaan Inggris. Salah satu karangannya yang paling terkenal adalah serial petualangan Sherlock Holmes, seorang detektif fiksi yang eksentrik.
+
+            Doyle dilahirkan pada tahun 1859. Ia mendapat gelar dokter dari Universitas Edinburgh dan mulai membuka praktik di Southsea, Inggris pada tahun 1882. Ia mengarang banyak cerita, dua diantaranya tidak pernah dipublikasikan.
+            
+            Pada tahun 1886, ia menciptakan tokoh Sherlock Holmes yang diilhami dari Dr. Joseph Bell, salah satu dosennya. Cerita pertama yang berjudul A Study in Scarlet (bahasa Indonesia: Penelusuran Benang Merah) ini diterima publik dengan baik. Akan tetapi, ketenaran tokoh itu baru dimulai pada tahun 1891 ketika ia menulis serial petualangan Sherlock Holmes bersama sahabat setianya, Dr. Watson, dalam bentuk kompilasi cerita pendek.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top" src="https://assets.editorial.aetnd.com/uploads/2014/07/gettyimages-178366909.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Pemulung</h5>
+        <p class="card-text col text-justify">Narasi Watson menggambarkan Holmes sebagai karakter yang sangat kompleks dan pemurung, meskipun memiliki kebiasaan yang ketat, namun sangat tidak rapi. Tempat tinggalnya di London di 221B, Baker Street, dirawat oleh pengurus rumah tangganya, Ny. Hudson. Holmes tampaknya mengalami serangan mania dan depresi, yang disertai dengan merokok pipa, bermain biola, dan penggunaan kokain . Sepanjang empat novel dan 56 cerita pendek yang menampilkan Holmes, sejumlah karakter muncul kembali, termasuk inspektur Scotland Yard Lestrade yang kikuk ; kelompok “orang Arab jalanan” yang dikenal sebagai Irregular Baker Street, yang secara rutin dipekerjakan oleh Holmes sebagai informan; saudaranya yang lebih bijaksana namun kurang ambisius, Mycroft; dan, yang paling menonjol, lawannya yang tangguh ,Profesor James Moriarty , yang dianggap Holmes sebagai “Napoleon kejahatan.”</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top" src="https://m.media-amazon.com/images/M/MV5BMzM3Y2JlMDMtM2E0Ny00YjA5LWJkOTYtNzk2OWY3NGE0YjBmXkEyXkFqcGdeQXVyMTMxNTMyNDQw._V1_.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Ngartis</h5>
+        <p class="card-text col text-justify">Mengklaim bahwa Holmes mengalihkan perhatiannya “dari hal-hal yang lebih baik,” Conan Doyle terkenal pada tahun 1893 (“Masalah Terakhir”) berusaha membunuhnya; selama perjuangan sengit di Air Terjun Reichenbach Swiss , Holmes dan musuh bebuyutannya , Profesor Moriarty, terjatuh ke tepi jurang . Kemarahan masyarakat terhadap kematian Holmes sangat besar; laki-laki mengenakan pita berkabung hitam, keluarga kerajaan Inggris putus asa , dan lebih dari 20.000 pembaca membatalkan langganan mereka ke Majalah Strand yang populer , tempat Holmes sering muncul. Atas permintaan banyak orang, Conan Doyle menghidupkan kembali detektifnya dalam cerita “Petualangan Rumah Kosong” (1903).</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+
+
+  
+
+  <?php include 'footer.php';?>
